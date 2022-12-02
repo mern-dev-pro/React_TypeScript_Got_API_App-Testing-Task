67410a5f0955debb5e0eba67916d510c45939b99
Mykhailo Romaniuk <mykhailo@nerdzz.digital>
2022-12-02 21:30:42 +0200

    CREATE: app basic structure

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Table from 'components/Table';
+import Button from 'components/Button';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Table />
+      <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utils/api.ts
@@ -0,0 +1,3 @@
+const fetchDataFromApi = async () => {};
+
+export { fetchDataFromApi };
