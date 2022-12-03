055e9836558fd7d065be38e2fb189ae9d81dd974
Mykhailo Romaniuk <mykhailo@nerdzz.digital>
2022-12-03 04:09:57 +0200

    CREATE: Table Component

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
 
-const Table = () => {
-  return <div>Table</div>;
+import styles from './styles.module.scss';
+
+type TableProps = {
+  data: { [key: string]: string }[];
+};
+
+const Table: React.FC<TableProps> = ({ data }) => {
+  return (
+    <div className={styles.wrapper}>
+      {!!data && data?.length > 0 && (
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              {Object.keys(data[0]).map((item: string) => (
+                <th key={item}>{item}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index: number) => (
+              <tr key={index}>
+                {Object.keys(data[index]).map((key, id) => (
+                  <td key={id}>{item[key]}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
 };
 
 export default Table;
