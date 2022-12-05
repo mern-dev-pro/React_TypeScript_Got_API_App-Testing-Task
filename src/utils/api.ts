ac7ad818cd819a3f289f3ff9d6246c771da2df36
Mykhailo Romaniuk <mykhailo@nerdzz.digital>
2022-12-05 08:56:30 +0200

    ADD: Call API for Character

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,3 +1,8 @@
-const fetchDataFromApi = async () => {};
+import axios from 'axios';
+
+const fetchDataFromApi = async (pageNum: number, pageSize: number) => {
+  const response = await axios.get(`https://anapioficeandfire.com/api/characters?page=${pageNum}&pageSize=${pageSize}`);
+  return response;
+};
 
 export { fetchDataFromApi };
